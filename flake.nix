ec160e77fb78e74bf16750efc89a25cf5b98da28
MayNiklas <info@niklas-steffen.de>
2023-01-03 18:06:46 +0100

    init latex2pdf

--- flake.nix
+++ flake.nix
@@ -1,18 +1,35 @@
 {
   description = "Abgabe Template";
-  outputs = { self }: {
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    # nix flake init --template 'github:alexanderwallau/Abgabe-template'
-    templates.default = self.templates.abgabe-templates;
-    templates.abgabe-templates = {
-      description = "Abgabe Template";
-      path = ./src;
-      welcomeText = ''
-        # Simple Template for University Assignments
-        ## More info
-        - [GitHub](https://github.com/alexanderwallau/Abgabe-template/)
-      '';
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = nixpkgs.lib.systems.flakeExposed;
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    rec{
+
+      packages = forAllSystems
+        (system: {
+          default = self.packages.${system}.latex2pdf;
+          latex2pdf = let pkgs = nixpkgs.legacyPackages.${system}; in
+            pkgs.writeShellScriptBin "latex2pdf" ''
+              ${pkgs.neofetch}/bin/neofetch
+            '';
+        });
+
+
+      # nix flake init --template 'github:alexanderwallau/Abgabe-template'
+      templates.default = self.templates.abgabe-templates;
+      templates.abgabe-templates = {
+        description = "Abgabe Template";
+        path = ./src;
+        welcomeText = ''
+          # Simple Template for University Assignments
+          ## More info
+          - [GitHub](https://github.com/alexanderwallau/Abgabe-template/)
+        '';
+      };
+
     };
-
-  };
 }
